100a4ba83180fff4e2ecf18340d59d64d729f26e
Michael Z <michael2012zhao@hotmail.com>
2019-02-13 13:42:15 +0000

    KMP algorithm, improve table.

--- string/kmp.py
+++ string/kmp.py
@@ -1,22 +1,19 @@
 
 def makeTable(p):
-    k = 0
-    c = 0
+    k = 0 # index of prefix, also the length of continuous matching substring
     table = list([None]*len(p))
     table[0] = -1
     for i in range(1, len(p)):
         if p[i] == p[k]:
-            c += 1
             table[i] = table[k]
             k += 1
         else:
-            table[i] = c
-            c = 0
+            table[i] = k
             k = 0
     return table
 
 if __name__ == "__main__":
-    p = "aaabaaa"
+    p = "abcabdabcaaa"
     table = makeTable(p)
     print(p)
     print(table)
