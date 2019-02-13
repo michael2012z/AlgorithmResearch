b084c37020daf75128add28131d8ef1c9e685254
Michael Z <michael2012zhao@hotmail.com>
2019-02-13 14:45:07 +0000

    KMP: a complete example.

--- string/kmp.py
+++ string/kmp.py
@@ -12,8 +12,30 @@ def makeTable(p):
             k = 0
     return table
 
+def kmpSearch(s, p):
+    '''
+    s -> string
+    p -> pattern
+    '''
+    t = makeTable(p)
+    i = 0
+    j = 0
+    while i < len(s):
+        if s[i] == p[j]:
+            i += 1
+            j += 1
+            if j == len(p):
+                return i - j
+        else:
+            i -= t[j]
+            j = 0
+    return -1
+
 if __name__ == "__main__":
     p = "abcabdabcaaa"
     table = makeTable(p)
     print(p)
     print(table)
+    print("++++++++++++")
+    print(kmpSearch("abcabcabdabc", "abcabd"))
+    
