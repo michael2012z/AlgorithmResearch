ddc8c0fe6327dc9ecb0c03583db0cf58e7780838
Michael Z <michael2012zhao@hotmail.com>
2019-02-15 04:35:19 +0000

    Add naive algorithm to compare.

--- string/kmp.py
+++ string/kmp.py
@@ -1,3 +1,22 @@
+''' Brute-force method as comparasion base. '''
+
+def naiveSearch (s, p):
+    i = 0
+    j = 0
+    result = []
+    while i < len(s):
+        if s[i] == p[j]:
+            i += 1
+            j += 1
+            if j == len(p):
+                result.append(i - j)
+                j = 0
+        else:
+            i -= j - 1
+            j = 0
+    return result
+
+'''++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
 
 def makeTableA(p):
     k = 0 # index of prefix, also the length of continuous matching substring
@@ -46,14 +65,25 @@ def kmpSearch(s, p):
             j = 0
     return result
 
-if __name__ == "__main__":
+'''++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++ '''
+''' Test '''
+
+def testNaiveSearch(s, p):
+    print(naiveSearch(s, p))    
+
+def testMakeTable():
     p = "abcabdabcaaa"
     table = makeTableB(p)
     print(p)
     print(table)
-    print("++++++++++++")
+
+def testKmpSearch(s, p):
+    print(kmpSearch(s, p))    
+
+if __name__ == "__main__":
     ruler = '0         1         2         3         4         5         6         7         8         '
     ruler = '012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789'
     s     = 'abcffsbcxfsabcabcabdabcabdaaaaabcabckfdliurewovnmxvbnabcabdabcabdndjklsabcabdabdabdfj'
-    print(kmpSearch(s, "abcabd"))
-    
+    p     = 'abcabd'
+    testNaiveSearch(s, p)
+    testKmpSearch(s, p)
