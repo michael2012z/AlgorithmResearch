828c5d3cc5064744d6e2855b515536b469869d87
Michael Z <michael2012zhao@hotmail.com>
2019-02-25 00:10:37 +0000

    Binary heap done.

--- algorithms/trees/binaryHeap.py
+++ algorithms/trees/binaryHeap.py
@@ -1,5 +1,4 @@
 
-
 class BinaryHeap:
     '''
     Min Heap
@@ -8,8 +7,13 @@ class BinaryHeap:
     heapList = []
     heapSize = 0
     
-    def __init__ (self):
-        pass
+    def __init__ (self, l = []):
+        self.heapList = l
+        self.heapSize = len(l)
+        i = (self.heapSize - 1 - 1) // 2 # last non-leaves node
+        while i > -1:
+            self.goDown(i)
+            i -= 1
 
     def minChild(self, i):
         l = i * 2 + 1 # left child
@@ -29,7 +33,7 @@ class BinaryHeap:
     
     def goUp(self, i):
         while i > 0:
-            p = i // 2 # parent
+            p = (i - 1) // 2 # parent
             if self.heapList[i] < self.heapList[p]:
                self.heapList[i], self.heapList[p] = self.heapList[p], self.heapList[i]
                i = p
@@ -41,17 +45,45 @@ class BinaryHeap:
             c = self.minChild(i)
             if c == -1:
                 break
-            else:
+            elif self.heapList[i] > self.heapList[c]:
                 self.heapList[i], self.heapList[c] = self.heapList[c], self.heapList[i]
                 i = c
+            else:
+                break
 
+    def insert(self, key):
+        self.heapList.append(key)
+        self.heapSize += 1
+        self.goUp(self.heapSize-1)
 
+    def remove(self):
+        top = self.heapList[0]
+        self.heapList[0] = self.heapList.pop()
+        self.heapSize -= 1
+        self.goDown(0)
+        return top
 
-      '''
-   0 
- 1   2
-3 4 5 6 
+    def size(self):
+        return self.heapSize
 
-i-1 // 2 
-      '''
+    def getList(self):
+        return self.heapList
+
       
+if __name__ == '__main__':
+    print('+++++++++++ test 1 ++++++++++++++')
+    l = [3, 5, 2, 6, 4, 1, 0]
+    bh = BinaryHeap(l)
+    print(bh.getList())
+    print('+++++++++++ test 2 ++++++++++++++')
+    l = [0, 1, 2, 3, 4, 5, 6]
+    bh = BinaryHeap(l)
+    print(bh.getList())
+    print('+++++++++++ test 3 ++++++++++++++')
+    l = [2, 3, 4, 5, 6, 7, 8]
+    bh = BinaryHeap(l)
+    bh.insert(9)
+    bh.insert(1)
+    bh.insert(0)
+    bh.remove()
+    print(bh.getList())
