f5465b80d8209e7426ef233aaed2dc996558b03f
Michael Z <michael.zhao@citrix.com>
2019-04-16 21:31:45 +1000

    Matrix Chain Multi.
    Signed-off-by: Michael Z <michael.zhao@citrix.com>

---
+++ dynamic-programming/matrix-chain-multiplication.py
@@ -0,0 +1,13 @@
+
+def mat_mul_recursive (dim, start, end):
+    
+
+
+if __name__ == "__main__":
+    # matrix list:
+    # Matrix       A1       A2       A3       A4       A5       A6
+    # Dimension    30x35    35x15    15x5     5x10     10x20    20x25
+    d = [30, 35, 15, 5, 10, 20, 25]
+    mat_mul = mat_mul_recursive
+    mat_mul(d, 1, len(d))
+    
