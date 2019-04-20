ea4b513c254d8e5baf6356a233165535a41e2c1d
Michael Z <michael2012zhao@hotmail.com>
2019-04-20 02:45:53 +0100

    working recursive algorithm
    Signed-off-by: Michael Z <michael2012zhao@hotmail.com>

--- dynamic-programming/matrix-chain-multiplication.py
+++ dynamic-programming/matrix-chain-multiplication.py
@@ -1,6 +1,15 @@
 
-def mat_mul_recursive (dim, start, end):
-    
+def mat_mul_recursive(dim, start, end):
+    '''
+    You have to pay extra attendition to the index.
+    '''
+    if start == end:
+        return 0
+    candidates = []
+    for k in range(start, end):
+        candidate = mat_mul_recursive(dim, start, k) + mat_mul_recursive(dim, k+1, end) + dim[start-1]*dim[k]*dim[end]
+        candidates.append(candidate)
+    return min(candidates)
 
 
 if __name__ == "__main__":
@@ -9,5 +18,6 @@ if __name__ == "__main__":
     # Dimension    30x35    35x15    15x5     5x10     10x20    20x25
     d = [30, 35, 15, 5, 10, 20, 25]
     mat_mul = mat_mul_recursive
-    mat_mul(d, 1, len(d))
+    x = mat_mul(d, 1, len(d)-1)
+    print(x)
     
