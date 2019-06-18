a33fe8aafb25bf7d9fb328170da4d187945b03f7
Michael Z <michael.zhao@citrix.com>
2019-06-18 17:03:32 +1000

    Visualize.

--- sorting/All.py
+++ sorting/All.py
@@ -1,4 +1,6 @@
 import random
+from graphviz import Digraph, nohtml
+import uuid
 
 class Sortee:
     ''' A key-value pair. '''
@@ -54,6 +56,28 @@ class Sorter:
             else:
                 printFunction(i)
 
+    def visualize(self, filename='tmp.gv'):
+        g = Digraph('g', filename=filename, node_attr={'shape': 'circle', 'height': '.1'})
+        priorNodeName = ""
+        for i in self.lst:
+            nodeName = str(uuid.uuid1())
+            g.node(nodeName, nohtml(str(i.key)))
+            if priorNodeName != "":
+                g.edge(priorNodeName, nodeName)
+            priorNodeName = nodeName
+        return g
+
+    def _visualize(self, g):
+        nodeName = str(uuid.uuid1())
+        g.node(nodeName, nohtml('<l> |<n> ' + str(self.value) + '|<r>'))
+        if self.leftChild != None:
+            leftUUID = self.leftChild._visualize(g)
+            g.edge(nodeName + ':l', leftUUID + ':n')
+        if self.rightChild != None:
+            rightUUID = self.rightChild._visualize(g)
+            g.edge(nodeName + ':r', rightUUID + ':n')
+        return nodeName
+
                 
 class BubbleSort(Sorter):
     def __init__(self, lst):
@@ -78,4 +102,6 @@ random.shuffle(a)
 s = BubbleSort(a)
 s.sort()
 s.print(lambda x: print(x.key))
-
+g = s.visualize()
+g.view()
+    
